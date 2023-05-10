6a659dda36a4670543642c491658e064d44244a6
Git-hub-Eduard <rabovolenkoeduard@gmail.com>
2023-05-10 23:14:29 +0300

    Практична робота №5

---
+++ ConsoleApp3/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static Thread thread;
+        static int people = 50;
+        static int sizeTrain = 10;
+        static int size = 0;
+        static object block = new object();
+        static void Main(string[] args)
+        {
+            takeRide(people);
+            Console.ReadLine();
+        }
+        public static void takeRide(int sizePesenger)
+        {
+            Console.WriteLine("Викликаємо вагончик");
+            for (int i = 1; i <= sizePesenger; i++)
+            {
+                thread = new Thread(load);
+                thread.Name = $"Пасажир {i}";
+                thread.Start();
+            }
+
+
+        }
+        public static void load()
+        {
+            Monitor.Enter(block);
+            Console.WriteLine($"{Thread.CurrentThread.Name} ciв до вагончика");
+            Thread.Sleep(1000);
+            unload();
+
+        }
+        public static void unload()
+        { 
+            Console.WriteLine($"{Thread.CurrentThread.Name} вийшов з вагончика");
+            Monitor.Exit(block);
+  
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static int  people = 50;
+        static int sizetrain = 5;
+        static int _sizetrain = 5;
+        static Semaphore semaphorePas;
+        static Thread  thread;
+        static void Main(string[] args)
+        {
+            int sizeSemaphore = sizetrain;
+            semaphorePas = new Semaphore(5, sizeSemaphore);
+            takeRide(people);
+            Console.ReadLine();
+
+        }
+        public static void takeRide(int sizePesenger )
+        {
+            Console.WriteLine("Викликаємо вагончик");
+            for(int i = 1; i <= sizePesenger; i++)
+            {
+                thread = new Thread(load);
+                thread.Name = $"Пасажир {i}";
+                thread.Start();
+            }
+            
+
+        }
+        public static void load()
+        {
+           
+           while(sizetrain>0)
+           {
+                semaphorePas.WaitOne();
+                Console.WriteLine($"{Thread.CurrentThread.Name} ciв до вагончика");
+                Thread.Sleep(2000);
+                unload();
+                sizetrain--;
+           }
+
+        }
+        public  static void unload()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} вийшов з вагончика");
+            Thread.Sleep(2000);
+            semaphorePas.Release();
+        }
+    }
+}
